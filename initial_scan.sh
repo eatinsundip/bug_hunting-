4c0eff2caa0319ef27bafbb11ebec183243e004b
eatinsundip <43767555+eatinsundip@users.noreply.github.com>
2023-08-18 09:41:09 -0500

    Update initial_scan.sh

--- initial_scan.sh
+++ initial_scan.sh
@@ -16,17 +16,15 @@ mkdir -p ~/recon/"$project"
 working_dir="~/recon/$Project"
 
 # Gather all subdomains related to the scope
-cat "$domain" | subfinder > ~/recon/$project/domains
+cat "$domain" | subfinder | anew > ~/recon/$project/domains
 
 #gather all known IPs related to domains
 input_file="~/recon/$project/domains"   # Replace with your input file name
-output_file="~/recon/$project/IPs" # Replace with your output file name
+output_file="~/recon/$project/hosts" # Replace with your output file name
 
 while IFS= read -r domain; do
-    ip=$(host -t A "$domain" | grep "has address" | awk '{print $NF}')
+    ip=$(host -t A "$domain"
     if [ -n "$ip" ]; then
-        echo "$ip" >> "$output_file"
-    else
-        echo "$domain: Could not resolve" >> "$output_file"
+        echo "$ip" | anew >> "$output_file"
     fi
 done < "$input_file"
