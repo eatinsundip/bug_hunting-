c3d566130d1ec6f5081ea5f2aed8b6594adc77bc
eatinsundip <43767555+eatinsundip@users.noreply.github.com>
2023-09-07 08:37:27 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# install for my version of Ubuntu but can add more support if interested.
+apt update && apt install golang-go -y
+go install github.com/tomnomnom/assetfinder@latest
+go install github.com/tomnomnom/httprobe@latest
+go install github.com/tomnomnom/anew@latest
+go install github.com/tomnomnom/fff@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/tomnomnom/gf@latest
+git clone https://github.com/tomnomnom/gf
+mkdir ~/.gf
+mv gf/examples/* ~/.gf
+rm -rf gf
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latestgo install -v github.com/projectdiscovery/notify/cmd/notify@latest
+go install -v github.com/projectdiscovery/nuclei/cmd/nuclei@latest
+
+echo "Finished"
