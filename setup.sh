440637b5ec8a8bc01735f79b45762abcc1e59348
eatinsundip <43767555+eatinsundip@users.noreply.github.com>
2023-09-07 09:04:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
+#check if arguement was provided.
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <Mullvad ID>"
+  exit 1
+fi
+
 # install for my version of Ubuntu but can add more support if interested.
 apt update && apt install golang-go -y
 go install github.com/tomnomnom/assetfinder@latest
@@ -8,16 +14,19 @@ go install github.com/tomnomnom/anew@latest
 go install github.com/tomnomnom/fff@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install github.com/tomnomnom/gf@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest
+go install -v github.com/projectdiscovery/nuclei/cmd/nuclei@latest
 git clone https://github.com/tomnomnom/gf
 mkdir ~/.gf
 mv gf/examples/* ~/.gf
 rm -rf gf
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latestgo install -v github.com/projectdiscovery/notify/cmd/notify@latest
-go install -v github.com/projectdiscovery/nuclei/cmd/nuclei@latest
 wget --content-disposition https://mullvad.net/download/app/deb/latest
 sudo apt install -y ./MullvadVPN-*_amd64.deb
 sudo rm -rf ./MullvadVPN-*_amd64.deb
-mullvad connect && mullvad status
+mullvad account login
+$1
+
 
 echo "Finished"
